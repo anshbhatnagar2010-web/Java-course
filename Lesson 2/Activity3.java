9addf0beec9e8e353cc9bb33903b3edaeaebe4c9
Ansh <anshbhatnagar2010@gmail.com>
2026-08-03 15:26:56 -0400

    L1 A3

---
+++ Lesson 2/Activity3.java
@@ -0,0 +1,15 @@
+class Activity3 {
+    public static void main(String[] args) {
+        System.out.println("========================My maggie recipe========================");
+        Maggie();
+    }
+    
+    public static void Maggie() {
+        System.out.println("Boil some water");
+        System.out.println("Add some Maggie Masala to the pot");
+        System.out.println("Add the Maggie noodles to your pot");
+        System.out.println("Let it cook for 2 minutes");
+        System.out.println("Serve it hot and enjoy your Maggie");
+        System.out.println("Ps: You can add some veggies or ketchup to your Maggie");
+    }
+}
