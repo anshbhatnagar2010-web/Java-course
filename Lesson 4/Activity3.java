4b541a0056d83d01ec02f8fff74bdaa562dd9a45
Ansh <anshbhatnagar2010@gmail.com>
2026-08-07 14:28:56 -0400

    M1 L4

--- Lesson 4/Activity3.java
+++ Lesson 4/Activity3.java
@@ -2,9 +2,6 @@ import java.util.Scanner;
 class Activity 3{
     public static void main(String[] args){
         Scanner scn = new Scanner (System.in);
-        int first = scn.nextInt();
-        int second = scn.nextInt();
-        String operator = scn.nextLine();
 
         System.out.println ("Enter first number: ");
         System.out.println ("Enter second number: ");
