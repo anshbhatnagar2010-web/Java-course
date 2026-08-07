e021efe4d318c2547350cec65ffbaef410f615af
Ansh <anshbhatnagar2010@gmail.com>
2026-08-07 13:39:45 -0400

    M1 L4
    Co-authored-by: Biswareet Panda <biswa.gunu2003@gmail.com>

---
+++ Lesson 4/Activity1.java
@@ -0,0 +1,16 @@
+public class Activity1 {
+    public static void main(String[] args){
+        boolean sunny=true;
+        boolean hot=false;
+            if(sunny){
+                if(hot){
+                    System.out.println("We will go out with cap and half sleeves");
+                }else{
+                    System.out.println("We will go out with half sleeves");
+                }
+            }else{
+                System.out.println("We will go out with raincoat");
+
+            }
+    }
+}
