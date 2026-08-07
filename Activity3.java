c6aa7e9b3a57fcfb6397067d86f3dabe1112892a
Ansh <anshbhatnagar2010@gmail.com>
2026-08-07 12:25:45 -0400

    L3 A3

---
+++ Activity3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Activity3 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter your assignment grade: ");
+        int grade = scn.nextInt();
+
+        if (grade > 90){
+            System.out.println("O");
+        }else if (grade > 80){
+            System.out.println("A+");
+        }else if (grade > 70){
+            System.out.println("A");
+        }else if (grade > 60){
+            System.out.println("B");
+        }else{
+            System.out.println("C");
+        }
+
+    }
+}
